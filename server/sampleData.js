79f5452f791519f5ad2cd2ffebcb2a87cd38f55c
raisul2 <raisul58208@gmail.com>
2022-12-02 19:06:16 +0600

    first commit

---
+++ server/sampleData.js
@@ -0,0 +1,77 @@
+const projects =[
+     {
+        id:"1",
+        clientId: '1',
+        name:"eComarce website",
+        description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, doloribus vitae quidem reprehenderit omnis dicta iste consequatur facilis similique, quam optio, et magnam possimus ea? Corrupti magnam incidunt vero reprehenderit.',
+         status:'In Progress'
+     },
+     {
+        id:"2",
+        clientId: '2',
+        name:"eComarce website",
+        description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, doloribus vitae quidem reprehenderit omnis dicta iste consequatur facilis similique, quam optio, et magnam possimus ea? Corrupti magnam incidunt vero reprehenderit.',
+         status:'In Progress'
+     },
+     {
+        id:"3",
+        clientId: '3',
+        name:"eComarce website",
+        description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, doloribus vitae quidem reprehenderit omnis dicta iste consequatur facilis similique, quam optio, et magnam possimus ea? Corrupti magnam incidunt vero reprehenderit.',
+         status:'In Progress'
+     },
+     {
+        id:"4",
+        clientId: '4',
+        name:"eComarce website",
+        description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, doloribus vitae quidem reprehenderit omnis dicta iste consequatur facilis similique, quam optio, et magnam possimus ea? Corrupti magnam incidunt vero reprehenderit.',
+         status:'In Progress'
+     },
+     {
+        id:"5",
+        clientId: '5',
+        name:"eComarce website",
+        description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, doloribus vitae quidem reprehenderit omnis dicta iste consequatur facilis similique, quam optio, et magnam possimus ea? Corrupti magnam incidunt vero reprehenderit.',
+         status:'In Progress'
+     },
+]
+
+const clients = [
+    {
+        id:'1',
+        name:'Tony stark',
+        email:'ironman@gamil.com',
+        phone:'3243-234-234',
+    },
+    {
+        id:'2',
+        name:'atik stark',
+        email:'atik@gamil.com',
+        phone:'3243-234-234',
+    },
+    {
+        id:'3',
+        name:'abdullah',
+        email:'abdullah@gamil.com',
+        phone:'3243-234-234',
+    },
+    {
+        id:'4',
+        name:'anisar',
+        email:'anisar@gamil.com',
+        phone:'3243-234-234',
+    },
+    {
+        id:'5',
+        name:'raisul ',
+        email:'raisul@gamil.com',
+        phone:'3243-234-234',
+    },
+]
+
+module.exports = {projects, clients}
